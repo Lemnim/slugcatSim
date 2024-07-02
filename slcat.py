faad894a8386e11019cc498227ef15dff16f092b
Lemnim <a01077255140@gmail.com>
2024-07-02 11:26:29 +0900

    Update slcat.py

--- slcat.py
+++ slcat.py
@@ -4,6 +4,7 @@ import time
 slCatHunger = 0
 evolutionLevel = 0
 friendship = 0
+morals = 4
 
 def evolution(hunger, evoLev):
     if hunger >= 4:
@@ -47,10 +48,32 @@ def slPupMake(evoLev, friship):
         print("슬러그캣은 아직 다른 슬러그캣에게 관심이 없어보인다...")
         return evoLev, friship
 
-
+def look(moral, friship):
+    friship += 20
+    print("슬러그캣은 자신을 관찰하는 당신에게 호감을 느낀다...")
+    time.sleep(1.5)
+    
+    if moral > 6:
+        friship -= 30
+        print("슬러그캣은 흙을 주워 먹는다!!!")
+        print("ʕx.xʔ: ! ! ! ! !")
+        return moral, friship
+    else:
+        moral += 1
+        print("슬러그캣은 다른 슬러그캣들과 놀고 있다.")
+        print("ʕ●.●ʔ: ? ?")
+        print("ʕ>.<ʔ: ! !")
+        return moral, friship
+        
+def study(moral, friship):
+    moral -= 3
+    friship -= 15
+    print("슬러그캣을 올바르게 교육시켰다. 그러면 안돼, 슬러그캣!")
+    print("ʕ~.~ʔ: ..!")
+    return moral, friship
 
 def pet(friship):
-    friship += 20
+    friship += 10
     print("슬러그캣을 쓰담쓰담...")
     print("ʕ●.●ʔ: ♥ ♥ ♥!")
     print(f"현재 호감도는 {friship}이다!")
@@ -58,8 +81,10 @@ def pet(friship):
 
 foods = {"박쥐파리" : 1, "팝콘" : 4}
 
+
+print("슬러그캣 시뮬레이터!")
 while True:
-    a = input("슬러그캣 시뮬레이터! 무엇을 할까? ")
+    a = input("무엇을 할까? ")
 
     if a == "먹이주기":
         b = input("어떤 음식을 먹일까? ")
@@ -69,7 +94,10 @@ while True:
         friendship = pet(friendship)
     elif a == "교배하기":
         evolutionLevel, friendship = slPupMake(evolutionLevel, friendship)
-        
+    elif a == "관찰하기":
+        morals, friendship = look(morals, friendship)
+    elif a == "교육하기":
+        morals, friendship = study(morals, friendship)
         
     else:
         break
