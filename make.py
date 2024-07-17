1afd4352dd6a5e0ebed4d84e2a75ad66226797a1
Lemnim <a01077255140@gmail.com>
2024-07-17 12:26:47 +0900

    Add files via upload

--- make.py
+++ make.py
@@ -3,7 +3,7 @@ import time
 
 items = {}
 money = 0
-haveItems = ["물뿌리개"]
+haveItems = []
 
 class Base(Enum):
     FARMING = 1
@@ -20,9 +20,12 @@ class Item:
         haveItems.append(name)
 
 # 아이템
+# 제작
 Item("빵", ["밀"], 80)
-Item("밀", [(Base.FARMING, 0, 5)], 10)
 
+# 기본 재료
+Item("밀", [(Base.FARMING, 0, 2)], 10)
+Item("우유", [(Base.LIVESTOCK, 10)])
 
 def sell(name, money, haveItems:list):
     if name in haveItems:
@@ -37,16 +40,21 @@ def sell(name, money, haveItems:list):
     return money, haveItems
 
 def craft(name, haveItems:list):
+    if name not in items.keys() or isinstance(items[name].material[0][0], Base):
+        print("제작할 수 없는 물건이다!")
+        return haveItems
+    
     item = items[name]
     for i in item.material:
-        if i in haveItems:
-            haveItems.remove(i)
-        else:
+        if i not in haveItems:
             print("재료가 부족해서 제작할 수 없다!")
-            return
-        
-    print(f"{name}을(를) 제작했다!")
+            return haveItems
+    
+    for i in item.material:
+        haveItems.remove(i)
+    
     haveItems.append(name)
+    print(f"{name}을(를) 제작했다!")
     return haveItems
 
 def status():
@@ -55,21 +63,24 @@ def status():
     
 def farm(name, money, haveItems:list):
     seed = items[name]
-    money -= seed.material[0][1]
-    print("씨앗이 자라나는 중...")
-    time.sleep(seed.material[0][2])
-    print(f"{name}을(를) 수확했다!")
-    haveItems.append(name)
-    return money, haveItems
+    if items[name].material[0][0] == Base.FARMING:
+        money -= seed.material[0][1]
+        print("씨앗이 자라나는 중...")
+        time.sleep(seed.material[0][2])
+        print(f"{name}을(를) 수확했다!")
+        haveItems.append(name)
+    else:
+        print("재배할 수 없는 것이다!")
     
+    return money, haveItems
 
 while True:
     a = input("무엇을 할까? ")
     
-    if a == "제작하기":
+    if a == "제작":
         b = input("무엇을 제작할까? ")
         haveItems = craft(b, haveItems)
-    elif a == "판매하기":
+    elif a == "판매":
         b = input("무엇을 판매할까? ")
         money, haveItems = sell(b, money, haveItems)
     elif a == "상태창":
